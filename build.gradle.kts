cc549d769f162eb0add2ed47934a000577e4ba26
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2026-02-18 20:34:06 +0100

    Bump Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.mmodding"
-version = "0.0.14-alpha"
+version = "0.0.15-alpha"
 val javaVersion = 17
 
 repositories {
--- src/test/groovy/com/mmodding/gradle/test/QuiltModJsonTest.groovy
+++ src/test/groovy/com/mmodding/gradle/test/QuiltModJsonTest.groovy
@@ -36,7 +36,7 @@ class QuiltModJsonTest extends Specification {
 		buildFile << """
 			plugins {
 				id("org.quiltmc.loom").version("1.8.+")
-				id("com.mmodding.gradle").version("0.0.14-alpha")
+				id("com.mmodding.gradle").version("0.0.15-alpha")
 			}
 
 			version = "0.0.1-test"
